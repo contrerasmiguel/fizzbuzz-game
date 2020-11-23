f5a2c5c3fb3e91b903bf0f9c10f535e051161ece
Miguel Contreras <migueldevelop@gmail.com>
2020-11-22 20:27:09 -0400

    Refactor

--- src/Util.hs
+++ src/Util.hs
@@ -7,5 +7,10 @@ import Data.List     (sortBy)
 reorderBy :: Ord b => [a] -> [b] -> [a]
 reorderBy xs = fmap fst . sortBy (compare `on` snd) . zip xs
 
-asManyAs :: (Applicative m, Foldable t) => t a-> m b -> m [b]
-asManyAs xs = replicateM $ length xs
+-- length returns an Int that is fed into replicateM, creating a new function
+-- that no longer requires an Int as first argument. It is only possible because
+-- the return value of length is Int, which is also the first parameter required
+-- by replicateM. The result is a function that requires t a (for length) and
+-- m a (for replicateM)
+asManyAs :: (Foldable t, Applicative m) => t a -> m b -> m [b]
+asManyAs = replicateM . length
